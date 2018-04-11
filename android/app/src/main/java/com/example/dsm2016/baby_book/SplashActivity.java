32f1e4a29744af272a2f4a3662a0c117ca495ab4
Hongsomang <ghdthakd12@naver.com>
2018-04-11 09:25:13 +0900

    .

---
+++ android/app/src/main/java/com/example/dsm2016/baby_book/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.dsm2016.baby_book;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ghdth on 2018-04-11.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
