cefe4c0865ead687102d246182fb7ee042f2f39f
Jeong-minji <alswl4152@naver.com>
2018-04-11 14:33:10 +0900

    login

--- android/app/src/main/java/com/example/dsm2016/baby_book/LoginActivity.java
+++ android/app/src/main/java/com/example/dsm2016/baby_book/LoginActivity.java
@@ -1,6 +1,4 @@
 package com.example.dsm2016.baby_book;
-
-<<<<<<< HEAD
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,20 +7,11 @@ import android.support.v7.app.AppCompatActivity;
  * Created by ghdth on 2018-04-11.
  */
 
-public class LoginActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 public class LoginActivity extends BaseActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
->>>>>>> a6fc0befa177da394b1e2690f4a3c886b7a0d8ed
     }
 }
