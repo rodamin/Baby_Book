a6fc0befa177da394b1e2690f4a3c886b7a0d8ed
Jeong-minji <alswl4152@naver.com>
2018-04-11 11:12:11 +0900

    폰트 적용

---
+++ android/app/src/main/java/com/example/dsm2016/baby_book/BaseActivity.java
@@ -0,0 +1,40 @@
+package com.example.dsm2016.baby_book;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class BaseActivity extends Activity {
+
+    private static Typeface typeface;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        if(typeface == null) {
+            typeface = Typeface.createFromAsset(this.getAssets(), "NanumBarunpenR.ttf");
+        }
+        setGlobalFont(getWindow().getDecorView());
+    }
+
+    private void setGlobalFont(View view) {
+        if(view != null) {
+            if(view instanceof ViewGroup) {
+                ViewGroup viewGroup = (ViewGroup)view;
+                int vgCnt = viewGroup.getChildCount();
+                for(int i = 0; i<vgCnt; i++) {
+                    View v = viewGroup.getChildAt(i);
+                    if(v instanceof TextView) {
+                        ((TextView) v).setTypeface(typeface);
+                    }
+                    setGlobalFont(v);
+                }
+            }
+        }
+    }
+
+}
