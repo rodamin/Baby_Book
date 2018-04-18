4dbfb19244249f30a0c84dc4120c162fcb39d6d0
Jeong-minji <alswl4152@naver.com>
2018-04-18 17:11:42 +0900

    story layout

---
+++ android/app/src/main/java/com/example/dsm2016/baby_book/CoupleStoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.dsm2016.baby_book;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoupleStoryActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_couple_story);
+    }
+}
